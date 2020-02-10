163cab7b3bd7665fa18848576f1ebde7adb22c21
ShadyElia <shady.elia@hotmail.com>
2020-02-10 20:56:24 +0200

    add custom action

--- actions.py
+++ actions.py
@@ -1,27 +1,57 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/core/actions/#custom-actions/
+import logging
+from datetime import datetime
+from typing import Any, Dict, List, Text, Union, Optional
+import json
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.forms import FormAction
+from rasa_sdk.events import (
+    SlotSet,
+    UserUtteranceReverted,
+    ConversationPaused,
+    EventType,
+)
+from demo.api import MailChimpAPI
+from demo.algolia import AlgoliaAPI
+from demo.discourse import DiscourseAPI
+from demo import config
+from demo.api import MailChimpAPI
+from demo.community_events import CommunityEvent
+from demo.gdrive_service import GDriveService
+
+logger = logging.getLogger(__name__)
 
 
-# This is a simple example for a custom action which utters "Hello World!"
+class ActionGreetUser(Action):
+    """Greets the user with/without privacy policy"""
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+    def name(self) -> Text:
+        return "action_greet_user"
+
+    def run(self, dispatcher, tracker, domain) -> List[EventType]:
+        intent = tracker.latest_message["intent"].get("name")
+        shown_privacy = tracker.get_slot("shown_privacy")
+        name_entity = next(tracker.get_latest_entity_values("name"), None)
+        if intent == "greet" or (intent == "enter_data" and name_entity):
+            if shown_privacy and name_entity and name_entity.lower() != "sara":
+                dispatcher.utter_message(template="utter_greet_name", name=name_entity)
+                return []
+            elif shown_privacy:
+                dispatcher.utter_message(template="utter_greet_noname")
+                return []
+            else:
+                dispatcher.utter_message(template="utter_greet")
+                dispatcher.utter_message(template="utter_inform_privacypolicy")
+                dispatcher.utter_message(template="utter_ask_goal")
+                return [SlotSet("shown_privacy", True)]
+        elif intent[:-1] == "get_started_step" and not shown_privacy:
+            dispatcher.utter_message(template="utter_greet")
+            dispatcher.utter_message(template="utter_inform_privacypolicy")
+            dispatcher.utter_message(template=f"utter_{intent}")
+            return [SlotSet("shown_privacy", True), SlotSet("step", intent[-1])]
+        elif intent[:-1] == "get_started_step" and shown_privacy:
+            dispatcher.utter_message(template=f"utter_{intent}")
+            return [SlotSet("step", intent[-1])]
+        return []
+
